75c7cec9fd941e60fbeac72cb300a08e7c9b154f
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-01-14 12:43:18 +0530

    added navbar, home and exercise components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
   return (
-    <div className="App">
-      hellow world
-    </div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exersice/:id" element={<ExerciseDetail />} />
+      </Routes>
+    </Box>
   );
 }
 
