bca62cf838b9bd0f89fbb5392f9a5ff1eea62775
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-01-15 21:31:50 +0530

    navbar done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/exersice/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
