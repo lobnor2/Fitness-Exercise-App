bf8d89f13542ec7183b76a7e809e0e8623688636
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-01-16 22:07:52 +0530

    done herobanner

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -18,16 +18,37 @@ const HeroBanner = () => {
       <Typography
         fontWeight="600"
         sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
       >
         Sweat, Smile <br />
         And Repeat
       </Typography>
-      <Typography fontSize="22px" lineHeight="35px" mb={2}>
+      <Typography fontSize="22px" lineHeight="35px" mb={4}>
         Check out the most effective exersices
       </Typography>
-      <Button variant="contained" color="error" href="#exercises">
+      <Button
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+        variant="contained"
+        color="error"
+        href="#exercises"
+      >
         Explore More Exercise
       </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{
+          opacity: 0.1,
+          display: {
+            lg: "block",
+            xs: "none",
+          },
+        }}
+        fontSize="200px"
+      >
+        Exercise
+      </Typography>
       <img
         src={HeroBannerImage}
         alt="hero banner"
