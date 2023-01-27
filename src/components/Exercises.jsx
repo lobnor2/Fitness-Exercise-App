f55e5ba7bac84b9d6a3bf5a6ca4f053b6aefab39
lobsang norbu <50455820+lobnor2@users.noreply.github.com>
2023-01-27 23:19:55 +0530

    added pagination

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
 
@@ -7,6 +7,14 @@ import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ setExercises, bodyPart, exercises }) => {
   console.log(exercises);
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisesperpage = 9;
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "110px" } }} mt="50px" p="20px">
       <Typography variant="h3" mb="46px">
@@ -21,6 +29,20 @@ const Exercises = ({ setExercises, bodyPart, exercises }) => {
         {exercises.map((exercise, index) => (
           <ExerciseCard key={index} exercise={exercise} />
         ))}
+      </Stack>
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesperpage)}
+            page={currentPage}
+            // onChange={(e) => paginate(e, value)} behind the scene
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
